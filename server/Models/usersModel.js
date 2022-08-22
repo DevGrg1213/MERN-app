544b2a910be6920a69c027fbbdf62a3e8fb594e4
Dev Gurung <theyev1213@gmail.com>
2022-08-22 18:07:31 +0545

    Created login and signup pages with backend auth

--- server/Models/usersModel.js
+++ server/Models/usersModel.js
@@ -39,4 +39,24 @@ userSchema.statics.signup = async function (email, password) {
   return user;
 };
 
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("Invalid Email or Password");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Invalid Email or Password");
+  }
+
+  return user;
+};
+
 module.exports = mongoose.model("user", userSchema);
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -19,7 +19,13 @@ exports.signupUser = async (req, res) => {
 };
 
 exports.loginUser = async (req, res) => {
-  res.status(200).json({
-    message: "user logged in",
-  });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
